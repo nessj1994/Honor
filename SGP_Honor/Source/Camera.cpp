7805d579ca900aeff8e038a4226b946974717cd9
Conor <Herdinstinct794@gmail.com>
2014-10-28 11:07:35 -0400

    Nothing

--- SGP_Honor/Source/Camera.cpp
+++ SGP_Honor/Source/Camera.cpp
@@ -35,7 +35,7 @@ void Camera::Draw(SGD::Rectangle _rect, SGD::Color _color)
 
 void Camera::DrawAnimation(SGD::Point position, float rotation, AnimTimeStamp& ts, bool flipped)
 {
-	AnimationEngine::GetInstance()->Render(position, rotation, ts, flipped, 1, m_ptCameraPosition);
+	AnimationEngine::GetInstance()->Render({ position.x , position.y }, rotation, ts, flipped, m_szZoomScale.width, m_ptCameraPosition);
 }
 
 void Camera::DrawString(std::string str, SGD::Point position)
@@ -49,14 +49,33 @@ void Camera::DrawTexture(SGD::Point position, float rotation, SGD::HTexture m_hI
 	float scaleX = 1;
 	if (flipped == true)
 		scaleX = -scaleX;
+
 	SGD::GraphicsManager::GetInstance()->DrawTexture(m_hImage,
-	{ position.x - m_ptCameraPosition.x, position.y - m_ptCameraPosition.y }, rotation, {}, {}, { scaleX, 1 });
+	{ position.x - m_ptCameraPosition.x, position.y - m_ptCameraPosition.y }, rotation, {}, {}, { m_szZoomScale.width, m_szZoomScale.height });
 }
+
+void Camera::DrawTextureSection(SGD::HTexture handle, SGD::Point position, SGD::Rectangle section, float rotation, SGD::Vector rotationOffset, SGD::Color color, SGD::Size scale)
+{
+	//SGD::Rectangle tempRect;
+
+	//tempRect.left = section.left / m_szZoomScale.width;
+	//tempRect.top = section.top / m_szZoomScale.height;
+	//tempRect.right = tempRect.left + (section.right - section.left);
+	//tempRect.bottom = tempRect.top + (section.bottom - section.top);
+
+
+
+	SGD::GraphicsManager::GetInstance()->DrawTextureSection(handle,
+	{	(position.x /** m_szZoomScale.width*/)	/*- Camera::GetInstance()->GetCameraPos().x*/, 
+		(position.y /** m_szZoomScale.height*/) /*- Camera::GetInstance()->GetCameraPos().y*/ },
+	section, rotation, rotationOffset, color, { m_szZoomScale.width, m_szZoomScale.height });
+}
+
 
 void Camera::Update(float _elapsedTime)
 {
-	m_ptCameraPosition.x = (m_pPlayer->GetPosition().x - Game::GetInstance()->GetScreenWidth() / 3) /** m_fScale*/  ; // Divide by Scale
-	m_ptCameraPosition.y = (m_pPlayer->GetPosition().y - Game::GetInstance()->GetScreenHeight() / 2) /** m_fScale*/   ;// Divide by Scale
+	m_ptCameraPosition.x = (m_pPlayer->GetPosition().x - Game::GetInstance()->GetScreenWidth()  / 3)  /** m_fScale*/; // Divide by Scale
+	m_ptCameraPosition.y = (m_pPlayer->GetPosition().y - Game::GetInstance()->GetScreenHeight() / 2)  /** m_fScale*/;// Divide by Scale
 
 }
 
