5caadd361dfd84ca475603e2873e8422658d1ac0
blackbird794 <blackbird794@msn.com>
2014-11-06 15:05:01 -0500

    continue 2-2 pre-merge

--- SGP_Honor/Source/Player.cpp
+++ SGP_Honor/Source/Player.cpp
@@ -47,6 +47,7 @@ Player::Player() : Listener(this)
 	Listener::RegisterForEvent("Screen2x4");
 	Listener::RegisterForEvent("Screen3x3");
 	Listener::RegisterForEvent("Screen2x1.5");
+	Listener::RegisterForEvent("Screen3x1.5");
 	Listener::RegisterForEvent("Screen1.5x2");
 	Listener::RegisterForEvent("Screen1.5x3");
 	Listener::RegisterForEvent("FINALBOSS");
@@ -1313,6 +1314,13 @@ void Player::HandleEvent(const SGD::Event* pEvent)
 	if (pEvent->GetEventID() == "Screen2x1.5")
 	{
 		m_fPanX = 2;
+		m_fPanY = 1.5;
+		Camera::GetInstance()->SetCameraCap(0);
+
+	}
+	if (pEvent->GetEventID() == "Screen3x1.5")
+	{
+		m_fPanX = 3;
 		m_fPanY = 1.5;
 		Camera::GetInstance()->SetCameraCap(0);
 
